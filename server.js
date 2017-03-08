150dd357fdd9d0647b0a3c3fd74d0ae9dc644c99
jesperorb <jesper.orb@zocom.se>
2017-03-08 13:13:51 +0100

    Removed port conversion to number for azure deploy This is dumb, so dumb

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var jsonServer = require('json-server');
 var server = jsonServer.create();
 var router = jsonServer.router('db.json');
 var middlewares = jsonServer.defaults();
-var port = Number(process.env.PORT || 3000);
+var port = process.env.PORT || 3000;
 server.use(middlewares);
 server.use(router);
 server.listen(port, function () {
