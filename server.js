9ec2bed7c4252c1c09c88d753d901b219ad666e8
Jesper Orb <jesper.orb@zocom.se>
2017-03-28 14:09:48 +0200

    Added delete route

--- server.js
+++ server.js
@@ -2,9 +2,23 @@ var jsonServer = require('json-server');
 var server = jsonServer.create();
 var router = jsonServer.router('db.json');
 var middlewares = jsonServer.defaults();
+var fs = require('fs');
 var port = process.env.PORT || 3000;
+
+
 server.use(middlewares);
+
+server.get('/delete', (req, res) => {
+  let data = JSON.stringify({ "articles":[] });
+  fs.writeFile('./db.json', data, 'utf-8', (error)=>{
+    if(error) res.send(error)
+    res.send('Success!');
+  }) 
+});
+
 server.use(router);
 server.listen(port, function () {
-  console.log('JSON Server is running')
+  console.log('\x1b[36mjson-server is running!');
 });
+
+
