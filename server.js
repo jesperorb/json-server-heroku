604213f4d2d26fc6791ed8f6194ec7ebd044f66a
Jesper Orb <jesper.orb@zocom.se>
2017-05-02 21:12:00 +0200

    Removed a test route and added example json

--- server.js
+++ server.js
@@ -5,20 +5,9 @@ var middlewares = jsonServer.defaults();
 var fs = require('fs');
 var port = process.env.PORT || 3000;
 
-
 server.use(middlewares);
-
-server.get('/delete', (req, res) => {
-  let data = JSON.stringify({ "articles":[] });
-  fs.writeFile('./db.json', data, 'utf-8', (error)=>{
-    if(error) res.send(error)
-    res.send('Success!');
-  }) 
-});
-
 server.use(router);
+
 server.listen(port, function () {
   console.log('\x1b[36mjson-server is running!');
 });
-
-
