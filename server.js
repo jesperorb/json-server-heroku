46a33b309663d78ba6dc2a186bd140fe065061b4
Wyatt Lansdale <7484339+wlans@users.noreply.github.com>
2018-09-30 12:22:29 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+const delay = 2000;
+
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
+
+server.use(jsonServer.bodyParser);
+server.use((req, res, next) => {
+  setTimeout(next, delay);
+});
 
 server.use(middlewares);
 server.use(router);
